bda3e34549b5c4bcd94beb79417523f1939872d2
Harsh Thacker <hthacker2106@gmail.com>
2025-05-15 19:10:37 +0530

    Add main.py file

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+def make_commits(days: int):
+    if days < 1:
+        return os.system('git push')
+    else:
+        dates = f'{days} days ago'
+
+    with open('data.txt','a') as file:
+        file.write(f"{dates}\n")
+
+    os.system('git add data.txt')
+
+    os.system('git commit --date="'+dates+'" -m "First Commit"')
+
+    return days*make_commits(days-1)
+
+make_commits(10)
